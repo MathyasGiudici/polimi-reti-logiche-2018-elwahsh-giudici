2a46606af404a6bc43d0a59fec5da9efbd94568b
Mathyas Giudici <mathyas.giudici@mail.polimi.it>
2017-12-22 16:44:57 +0100

    Initial commit

---
+++ project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
@@ -0,0 +1,51 @@
+----------------------------------------------------------------------------------
+-- Company: El Wahsh - Giudici
+-- Engineer: 
+-- 
+-- Create Date: 22.12.2017 16:40:01
+-- Design Name: 
+-- Module Name: project_reti_logiche - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity project_reti_logiche is
+    Port ( i_clk : in STD_LOGIC;
+           i_start : in STD_LOGIC;
+           i_rst : in STD_LOGIC;
+           i_data : in STD_LOGIC_VECTOR (7 downto 0);
+           o_address : out STD_LOGIC_VECTOR (15 downto 0);
+           o_done : out STD_LOGIC;
+           o_en : out STD_LOGIC;
+           o_we : out STD_LOGIC;
+           o_data : out STD_LOGIC_VECTOR (7 downto 0));
+end project_reti_logiche;
+
+architecture Behavioral of project_reti_logiche is
+
+begin
+
+
+end Behavioral;
